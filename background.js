ec818e1f458ec82f9b184b80364a52b4f31469c7
tom reeder <thomaseugenereeder@gmail.com>
2021-04-28 20:29:16 -0700

    more stuff

--- background.js
+++ background.js
@@ -1,10 +1,23 @@
 chrome.runtime.onInstalled.addListener(function () {
-  // console.log('Hello World!!!!');
-  // const body = window.docume
-  // const newSpan = document.createElement('div');
-  // newSpan.innerHTML = 'WOWOW';
-  // body.appendChild(newSpan);
+  console.log('Hello World!!!!');
+  console.log('chrome.windows', chrome.windows);
 });
+
+function insertDiv() {
+    const body = document.getElementByTagName('body');
+    const newSpan = document.createElement('div');
+    newSpan.innerHTML = 'Search For Me';
+    body.appendChild(newSpan);
+}
+
+chrome.action.onClicked.addListener((tab) => {
+  console.log(tab);
+    chrome.scripting.executeScript({
+        target: {tabId: tab.id},
+        function: insertDiv
+    })
+})
+
 
 // const dictionarySearch = (keyword) => {
 //   const url = `https://api-portal.dictionary.com/dcom/pageData/${keyword}`;
---
+++ content.js
@@ -0,0 +1,7 @@
+// function injectStuff () {
+//   const body = document.getElementById('body');
+//   const newSpan = document.createElement('div');
+//   newSpan.innerHTML = 'Search For Me';
+//   body.appendChild(newSpan);
+//   console.log(newSpan);
+// }
