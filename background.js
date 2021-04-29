a6664ef7ff4d46fbbbbf80505199a9f5057cb7bb
tom reeder <thomaseugenereeder@gmail.com>
2021-04-28 19:36:17 -0700

    stuff

---
+++ background.js
@@ -0,0 +1,16 @@
+chrome.runtime.onInstalled.addListener(function () {
+  // console.log('Hello World!!!!');
+  // const body = window.docume
+  // const newSpan = document.createElement('div');
+  // newSpan.innerHTML = 'WOWOW';
+  // body.appendChild(newSpan);
+});
+
+// const dictionarySearch = (keyword) => {
+//   const url = `https://api-portal.dictionary.com/dcom/pageData/${keyword}`;
+// }
+
+// chrome.runtime.onInstalled.addListener(function () {
+  // chrome.contextMenus.create(createProperties: {}, dictionarySearch);
+// 
+// });
